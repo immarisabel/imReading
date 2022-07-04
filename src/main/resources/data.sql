210929fed7de7fac593729ab10f391bcf4394edb
Marisabel <72731147+immarisabel@users.noreply.github.com>
2022-07-04 10:36:04 +0200

    data loading properly

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,9 @@
-insert into BOOKS(id, OLid, title, author, thumbnail_url, short_description, status, rating, favorite, start_date, finished_date)
+insert into books(id, OLid, title, author, thumbnail_url, short_description, status, rating, favorite, start_date, finished_date)
 values
 (1, 123, 'Life', 'xxx', 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg?ts=1637008457', 'xxx', 'reading', 0, 0, 'xxx', 'xxx'),
 (2, 456, 'Joy', 'xxx', 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg?ts=1637008457', 'xxx', 'reading', 0, 0, 'xxx', 'xxx');
 
-
+insert into logs(id, OLid,book,shelf,content,date,mood,page)
+values
+(1,123, 'Life', 'xxx', 'test LIFE entry', 'xxx', 'xxx', 1),
+(2,456, 'Joy', 'xxx', 'test JOY entry', 'xxx', 'xxx', 1);
--- src/main/resources/data_logs.sql
+++
@@ -1,4 +0,0 @@
-insert into LOGS(id, OLid,book,shelf,content,date,mood,page)
-values
-(1,123, 'Life', 'xxx', 'test LIFE entry', 'xxx', 'xxx', 1),
-(1,456, 'Joy', 'xxx', 'test JOY entry', 'xxx', 'xxx', 1);
